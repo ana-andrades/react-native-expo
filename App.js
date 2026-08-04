5ec41f6beac85dace50df411d5f18f99b0f790d2
Ana Julia <anajuliaasantana@gmail.com>
2026-08-04 20:03:48 -0300

    update card

--- App.js
+++ App.js
@@ -7,51 +7,49 @@ export default function App() {
       <View style={styles.box}>
         <Image
           style={styles.tinyLogo}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
+          source={'https://reactnative.dev/img/tiny_logo.png'}
         />
-        <Text style={styles.textLogo}>APP Legal</Text>
+        <Text style={styles.textLogo}>Orfanato Raio de Luz</Text>
       </View>
 
       <ScrollView style={styles.box2} contentContainerStyle={styles.cardsContainer}>
 
-<View style={styles.card}>
-          <Image 
-            style={styles.cardImage} 
-            source={{ uri: 'https://s2.glbimg.com/63hqcNFjh5efwqCFyBqqBCVyPkLYffw3GZZAjbxSJutIoz-HdGixxa_8qOZvMp3w/e.glbimg.com/og/ed/f/original/2013/10/23/rayssa2.jpg' }} 
+        <View style={styles.card}>
+          <Image
+            style={styles.cardImage}
+            source={'https://s2.glbimg.com/63hqcNFjh5efwqCFyBqqBCVyPkLYffw3GZZAjbxSJutIoz-HdGixxa_8qOZvMp3w/e.glbimg.com/og/ed/f/original/2013/10/23/rayssa2.jpg'}
           />
           <View style={styles.cardContent}>
-            <Text style={styles.cardTitle}>Beatriz Órfã</Text>
+            <Text style={styles.cardTitle}>Bia</Text>
             <Text style={styles.cardDescription}>bia@gmail.com</Text>
           </View>
         </View>
 
         <View style={styles.card}>
-          <Image 
-            style={styles.cardImage} 
-            source={{ uri: 'https://img.observatoriodatv.com.br/wp-content/uploads/2020/12/thiago-pedro-henrique-chiquititas.jpg' }} 
+          <Image
+            style={styles.cardImage}
+            source={'https://img.observatoriodatv.com.br/wp-content/uploads/2020/12/thiago-pedro-henrique-chiquititas.jpg'}
           />
           <View style={styles.cardContent}>
-            <Text style={styles.cardTitle}>Thiago Órfã</Text>
+            <Text style={styles.cardTitle}>Thiago</Text>
             <Text style={styles.cardDescription}>thiago@gmail.com</Text>
           </View>
         </View>
 
         <View style={styles.card}>
-          <Image 
-            style={styles.cardImage} 
-            source={{ uri: 'https://ofuxico.com.br/img/upload/novelas_interna/2021/05/17/marian-chiquititas_19386_76.jpg' }} 
+          <Image
+            style={styles.cardImage}
+            source={'https://ofuxico.com.br/img/upload/novelas_interna/2021/05/17/marian-chiquititas_19386_76.jpg'}
           />
           <View style={styles.cardContent}>
-            <Text style={styles.cardTitle}>Marian Órfã</Text>
+            <Text style={styles.cardTitle}>Marian</Text>
             <Text style={styles.cardDescription}>mari@gmail.com</Text>
           </View>
         </View>
 
       </ScrollView>
 
-       <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -74,26 +72,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#182446'
   },
   cardsContainer: {
-    padding: 20, 
-    gap: 15,    
+    padding: 20,
+    gap: 15,
   },
   card: {
     backgroundColor: '#ffffff',
     height: 100,
     width: '100%',
-    borderRadius: 12,     
-    padding: 16,   
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    gap: 15,         
+    borderRadius: 12,
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
   },
   cardImage: {
-    width: 60,       
-    height: 60,      
-    borderRadius: 30,     
+    width: 60,
+    height: 60,
+    borderRadius: 30,
   },
   cardContent: {
-    justifyContent: 'center', 
+    justifyContent: 'center',
   },
   cardTitle: {
     fontSize: 18,
